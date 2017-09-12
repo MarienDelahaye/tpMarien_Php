92fb1cb67c1f7c1789ef91020c086ba07670d200
Delahaye Marien <delahaye.marien@gmail.com>
2017-09-12 11:08:10 +0200

    Added content in secret.php

---
+++ secret.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Code d'accès de l'entreprise</title>
+    </head>
+    <body>
+    
+        <?php
+    {
+    // On affiche les codes
+    ?>
+        <h1>Voici le code d'accès :</h1>
+        <p><strong>AZER1-TYUI5-WASD3</strong></p>   
+        
+        <p>
+        Réservé au personnel de l'entreprise.<br />
+        </p>
+        <?php
+    }
+    else 
+    {
+    
+    }
+    ?>
+    
+        
+    </body>
+</html>
